f19d1b308985a672819d4575e092420ee93a4aea
mariumfirdous <40921093+mariumfirdous@users.noreply.github.com>
2021-04-15 20:19:47 +0530

    Add files via upload
    In this to do app we can add tasks by putting it in the textbox and press on the add button . once all the tasks are listed, we can click on tick to mark it as completed and also edit it by pressing on the edit button and delete it by pressing on the delete button.
    We can see all the completed tasks by clicking on the green completed tasks button and also can be undone by pressing on the undo button.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todoApp';
+
+      items : string[] = [];
+      newTask: string='';
+      completedTaskArray: string[] = [];
+      completedStatus: boolean=false;
+      showCompletedTask: boolean = false;
+     
+       public addToList() {
+           if (this.newTask == '') {
+           }
+           else {
+               this.items.push(this.newTask);
+               this.newTask = '';
+           }
+       }
+       public updateTask(index:any){
+         this.newTask = this.items[index];
+         this.items.splice(index, 1);
+       }
+
+       public deleteTask(index:any) {
+           this.items.splice(index, 1);
+       }
+
+       completedTask(index:any){
+         this.completedStatus = !this.completedStatus;
+               this.completedTaskArray.push(this.items[index]);
+               this.items.splice(index,1);
+         
+       }
+       showCompletedTasks(){
+         this.showCompletedTask = !this.showCompletedTask;
+       }
+
+       undoTask(index:any){
+          this.items.push(this.completedTaskArray[index]);
+          this.completedTaskArray.splice(index,1);
+       }
+       
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule } from '@angular/forms';
+import {MatTabsModule} from '@angular/material/tabs';
+
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    MatTabsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
